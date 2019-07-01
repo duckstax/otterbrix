fd32191f02b5ac17e24a917964cc1428c1d07571
kotbegemot <k0tb9g9m0t@yandex.ru>
2019-07-01 14:16:18 +0300

    object_storage temporarily disabled build

--- apps/rocketjoed/init_service.cpp
+++ apps/rocketjoed/init_service.cpp
@@ -7,7 +7,7 @@
 #include <rocketjoe/services/python_engine/python_engine.hpp>
 #include <rocketjoe/services/flat_cache/flat_cache.hpp>
 #include <rocketjoe/services/router/router.hpp>
-#include <rocketjoe/services/object_storage/object_storage.hpp>
+//#include <rocketjoe/services/object_storage/object_storage.hpp>
 #include <rocketjoe/http/server.hpp>
 
 
@@ -18,7 +18,7 @@ void init_service(goblin_engineer::dynamic_environment&env) {
         auto& python = env.add_service<rocketjoe::services::python_engine::python_engine>();
 /// rewrite config
 
-        auto& object_storage = env.add_service<rocketjoe::services::object_storage::object_storage>();
+//        auto& object_storage = env.add_service<rocketjoe::services::object_storage::object_storage>();
         auto& cache = env.add_service<rocketjoe::services::flat_cache::flat_cache>();
         auto& router = env.add_service<rocketjoe::services::router::router>();
 
@@ -28,13 +28,13 @@ void init_service(goblin_engineer::dynamic_environment&env) {
         router->add_shared(http.address().operator->());
         lua->add_shared(http.address().operator->());
         python->add_shared(http.address().operator->());
-        object_storage->add_shared(http.address().operator->());
+//       object_storage->add_shared(http.address().operator->());
 
-        actor_zeta::environment::link(router,object_storage);
+//       actor_zeta::environment::link(router,object_storage);
         actor_zeta::environment::link(router,lua);
         actor_zeta::environment::link(router,python);
-        actor_zeta::environment::link(lua,object_storage);
-        actor_zeta::environment::link(python,object_storage);
+//      actor_zeta::environment::link(lua,object_storage);
+//     actor_zeta::environment::link(python,object_storage);
 
         router->join(cache);
         lua->join(cache);
