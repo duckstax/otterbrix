3529fb227f526232d3e4bb2f2b54e297497b64c9
kotbegemot <aa.borgardt@yandex.ru>
2019-08-14 12:07:11 +0300

    removed 1)control_block 2)cache 3)object_storage

--- apps/rocketjoed/init_service.cpp
+++ apps/rocketjoed/init_service.cpp
@@ -1,15 +1,10 @@
 #include "init_service.hpp"
 
-#include <actor-zeta/environment/group.hpp>
 #include <actor-zeta/environment/cooperation.hpp>
 
 #include <rocketjoe/services/python_engine/python_engine.hpp>
-#include <rocketjoe/services/flat_cache/flat_cache.hpp>
 #include <rocketjoe/services/router/service_router.hpp>
 #include <rocketjoe/services/http_server/server.hpp>
-#include <rocketjoe/services/router/router.hpp>
-#include <rocketjoe/services/control_block/control_block.hpp>
-
 
 void init_service(goblin_engineer::dynamic_environment&env) {
 
@@ -19,10 +14,7 @@ void init_service(goblin_engineer::dynamic_environment&env) {
 /// rewrite config
 
 
-//      auto& object_storage = env.add_service<rocketjoe::services::object_storage::object_storage>();
-        auto& cache = env.add_service<rocketjoe::services::cache>();
         auto& router = env.add_service<rocketjoe::services::http_dispatcher>();
-        auto& control_block =env.add_service<rocketjoe::services::control_block>();
         auto& http = env.add_data_provider<rocketjoe::network::server>(router->entry_point());
 
         router->add_shared(http.address().operator->());
@@ -30,13 +22,6 @@ void init_service(goblin_engineer::dynamic_environment&env) {
         python->add_shared(http.address().operator->());
 //      object_storage->add_shared(http.address().operator->());
 
-//      actor_zeta::environment::link(router,object_storage);
 //      actor_zeta::environment::link(router,lua);
         actor_zeta::environment::link(router,python);
-//      actor_zeta::environment::link(lua,object_storage);
-//      actor_zeta::environment::link(python,object_storage);
-        actor_zeta::environment::link(router,control_block);
-        router->join(cache);
-//      lua->join(cache);
-
 }
