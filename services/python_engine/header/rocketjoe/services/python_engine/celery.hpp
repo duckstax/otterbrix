fdf712be9745e3ac6eb33646a3dfdb4dd965d869
kotbegemot <k0tb9g9m0t@yandex.ru>
2019-10-18 10:37:58 +0300

    fix 5

--- services/python_engine/header/rocketjoe/services/python_engine/celery.hpp
+++ services/python_engine/header/rocketjoe/services/python_engine/celery.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <boost/dll.hpp>
+
 #include <pybind11/stl.h>
 #include <pybind11/functional.h>
 #include <pybind11/pybind11.h>
@@ -9,6 +11,34 @@ namespace rocketjoe { namespace services { namespace python_engine {
             using namespace pybind11::literals;
             namespace py = pybind11;
             using py_task = py::function;
+
+            class BOOST_SYMBOL_VISIBLE result_base {};
+
+            class BOOST_SYMBOL_VISIBLE eager_result : public result_base {
+            public:
+                eager_result(py::object result);
+
+                auto get() -> py::object const;
+
+            private:
+                py::object result;
+            };
+
+            class BOOST_SYMBOL_VISIBLE task {
+            public:
+
+                task(py_task task_handler, std::string &&name);
+
+                auto operator()(py::args args, py::kwargs kwargs) -> eager_result const;
+
+            private:
+                py_task task_handler;
+            };
+
+            class celery {
+            public:
+                auto create_task() -> std::function<task(py_task)> const;
+            };
 
             void add_celery(py::module &pyrocketjoe);
 
