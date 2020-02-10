8d9d071ecab022522d5eef82b5f26d95803a5941
kotbegemot <k0tb9g9m0t@yandex.ru>
2020-02-10 11:15:13 +0300

    init

---
+++ services/header/rocketjoe/services/process_pool/process_pool.cpp
@@ -0,0 +1,1 @@
+#include <rocketjoe/services/process_pool/process_pool.hpp>
---
+++ services/header/rocketjoe/services/process_pool/process_pool.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <goblin-engineer.hpp>
+
+class process_pool_t final : goblin_engineer::abstract_service {
+
+};
