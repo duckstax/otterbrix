7e1252fb898806cd2e1a5e41ff13434721cdb327
kotbegemot <k0tb9g9m0t@yandex.ru>
2020-02-16 02:55:54 +0300

    fix log

--- core/rocketjoe/log/log.cpp
+++ core/rocketjoe/log/log.cpp
@@ -1,12 +1,15 @@
-
 #include "log.hpp"
 
-log::log(std::shared_ptr<spdlog::async_logger> logger) : logger_(logger) {}
+namespace rocketjoe {
 
-auto log::clone() noexcept -> log {
-    return logger_;
-}
+    log::log(std::shared_ptr<spdlog::async_logger> logger) : logger_(logger) {}
 
-auto log::context(std::shared_ptr<spdlog::async_logger> logger) noexcept -> void {
-    logger_= logger;
+    auto log::clone() noexcept -> log {
+        return logger_;
+    }
+
+    auto log::context(std::shared_ptr<spdlog::async_logger> logger) noexcept -> void {
+        logger_= logger;
+    }
+
 }
