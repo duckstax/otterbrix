3796438cf8bdce3a9fe94964f59aa7db9b0cee90
kotbegemot <k0tb9g9m0t@yandex.ru>
2020-02-15 22:14:02 +0300

    #57

--- cmake/clang_tools.cmake
+++ cmake/clang_tools.cmake
@@ -27,19 +27,19 @@ option(ADDRESS_SANITIZER "Enable Clang AddressSanitizer" OFF)
 
 if (ADDRESS_SANITIZER)
     message(STATUS "AddressSanitizer enabled for build")
-    set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O1 -fno-omit-frame-pointer -fsanitize=address")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O1 -fno-omit-frame-pointer -fsanitize=address")
 endif ()
 
 option(UNDEFINED_SANITIZER "Enable Clang UndefinedBehaviorSanitizer" OFF)
 
 if (UNDEFINED_SANITIZER)
     message(STATUS "UndefinedBehaviorSanitizer enabled for build")
-    set(CMAKE_CXX_FLAGS_DEBUG  "${CMAKE_CXX_FLAGS_DEBUG} -fsanitize=undefined -fsanitize=integer")
+    set(CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} -fsanitize=undefined -fsanitize=integer")
 endif ()
 
 option(CLANG_CODE_COVERAGE "Enable code coverage metrics in Clang" OFF)
 
 if (CLANG_CODE_COVERAGE)
     message(STATUS "Code coverage metrics enabled for build")
-    set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -fprofile-instr-generate -fcoverage-mapping")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fprofile-instr-generate -fcoverage-mapping")
 endif ()
