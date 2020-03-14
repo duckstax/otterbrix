cddd0bf29c0ac3a3d35a5bfcbb5f489faf5716a5
kotbegemot <k0tb9g9m0t@yandex.ru>
2020-03-14 16:56:13 +0300

    clear

--- core/rocketjoe/network/forward.hpp
+++ core/rocketjoe/network/forward.hpp
@@ -7,8 +7,5 @@ namespace rocketjoe { namespace network {
 
     using helper_write_f_t = std::function<void(network::request_type && , std::size_t)>;
     using boost::string_view;
-    ///using rocketjoe::http::http_method;
-    ///using rocketjoe::http::options;
-    ///using rocketjoe::http::query_context;
 
 }}
