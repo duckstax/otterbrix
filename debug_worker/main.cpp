3b4503d447c08fe36dc9f05535355f38c5e430d3
kotbegemot <k0tb9g9m0t@yandex.ru>
2020-03-24 18:29:26 +0300

    mini refactoring

--- debug_worker/main.cpp
+++ debug_worker/main.cpp
@@ -1,61 +1,66 @@
-#include <string>
-#include <iostream>
-#include <fstream>
 #include <chrono>
 #include <ctime>
+#include <fstream>
+#include <iostream>
+#include <string>
 #include <vector>
 
-template <class T>
-void logo(T&stream) {
+#include <boost/process/environment.hpp>
 
-    stream << std::endl;
+#include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/spdlog.h"
 
-    stream << "-------------------------------------------------";
-
-    stream << "\n"
-                 "\n"
-                 "       __     __                                       __   \n"
-                 "  ____/ /__  / /_  __  ______ _   ____ ___  ____  ____/ /__ \n"
-                 " / __  / _ \\/ __ \\/ / / / __ `/  / __ `__ \\/ __ \\/ __  / _ \\\n"
-                 "/ /_/ /  __/ /_/ / /_/ / /_/ /  / / / / / / /_/ / /_/ /  __/\n"
-                 "\\__,_/\\___/_.___/\\__,_/\\__, /  /_/ /_/ /_/\\____/\\__,_/\\___/ \n"
-                 "                      /____/                                "
-                 "                                                 \n"
-                 "                                                 \n"
-                 "";
-
-
-    stream << "-------------------------------------------------";
-
-    stream << std::endl;
-    stream << std::endl;
-
-    ////std::cout.flush();
-
+auto logo() -> const char * {
+  constexpr const char *data = R"_(
+-------------------------------------------------
+       __     __                                       __
+  ____/ /__  / /_  __  ______ _   ____ ___  ____  ____/ /__
+ / __  / _ \/ __ \/ / / / __ `/  / __ `__ \/ __ \/ __  / _ \
+/ /_/ /  __/ /_/ / /_/ / /_/ /  / / / / / / /_/ / /_/ /  __/
+\__,_/\___/_.___/\__,_/\__, /  /_/ /_/ /_/\____/\__,_/\___/
+                      /____/
+-------------------------------------------------
+)_";
+  return data;
 }
 
 int main(int argc, char *argv[]) {
-    std::ofstream myfile;
+  std::ofstream myfile;
 
-    myfile.open ("example.txt");
+  auto id = boost::this_process::get_id();
+  std::string filename;
+  ////example: "logs/basic.txt"
+  filename = std::string("logs/").append(std::to_string(id)).append(".txt");
+  auto file_logger = spdlog::basic_logger_mt("basic_logger", filename);
+  spdlog::set_default_logger(file_logger);
 
-    auto start = std::chrono::system_clock::now();
-    auto end = std::chrono::system_clock::now();
+  auto start = std::chrono::system_clock::now();
+  auto end = std::chrono::system_clock::now();
 
-    std::chrono::duration<double> elapsed_seconds = end-start;
-    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-    logo(myfile);
-    myfile  << "finished computation at " << std::ctime(&end_time)
-            << "elapsed time: " << elapsed_seconds.count() << "s\n";
+  std::chrono::duration<double> elapsed_seconds = end - start;
+  std::time_t end_time = std::chrono::system_clock::to_time_t(end);
 
-    std::vector<std::string> all_args(argv, argv + argc);
+  spdlog::info(logo());
 
-    for(const auto&i:all_args ){
-        myfile  << i << std::endl;
-    }
+  {
+    std::string message;
+    message.append("finished computation at ")
+        .append(std::ctime(&end_time))
+        .append("elapsed time: ")
+        .append(std::to_string(elapsed_seconds.count()))
+        .append("s");
+    spdlog::info(message);
+  }
 
-    myfile.close();
+  std::vector<std::string> all_args(argv, argv + argc);
 
-    return 0;
+  int j = 0;
+  for (const auto &i : all_args) {
+    std::string message;
+    message.append("Arg").append(std::to_string(j)).append(" : ").append(i);
+    spdlog::info(message);
+    ++j;
+  }
 
+  return 0;
 }
