78ed5e7ca4255170638298db6cc46a2efab9a4f6
Alexander <k0tb9g9m0t@yandex.ru>
2020-05-04 22:11:50 +0300

    class python_sandbox add method init and start
    * project dependency updated library goblin-engineer 1.0.0a3 
    
    * tests in project are now run via ctest

--- core/rocketjoe/python_sandbox/python_sandbox.hpp
+++ core/rocketjoe/python_sandbox/python_sandbox.hpp
@@ -8,17 +8,19 @@
 
 #include <pybind11/embed.h>
 
-#include <goblin-engineer/abstract_service.hpp>
+#include <goblin-engineer/abstract_manager_service.hpp>
+#include <goblin-engineer/components/root_manager.hpp>
 
-#include <rocketjoe/network/server.hpp>
 #include <rocketjoe/python_sandbox/detail/forward.hpp>
 #include <rocketjoe/python_sandbox/detail/jupyter/pykernel.hpp>
+
+#include <nlohmann/json.hpp>
 
 namespace rocketjoe { namespace services {
     namespace py = pybind11;
     using detail::jupyter::pykernel;
 
-    enum class sandbox_mode : std::uint8_t {
+    enum class sandbox_mode_t : std::uint8_t {
         none = 0,
         script = 1,
         jupyter_kernel = 2,
@@ -27,25 +29,25 @@ namespace rocketjoe { namespace services {
 
     class BOOST_SYMBOL_VISIBLE python_sandbox_t final : public goblin_engineer::abstract_manager_service {
     public:
-        python_sandbox_t(
-          goblin_engineer::root_manager *,
-          goblin_engineer::dynamic_config &
-        );
-
-      void enqueue(goblin_engineer::message , actor_zeta::executor::execution_device *) override;
+        python_sandbox_t(goblin_engineer::components::root_manager*, nlohmann::json&);
 
         ~python_sandbox_t() override = default;
 
-    private:
-        auto jupyter_kernel_init() -> void;
-
-        auto jupyter_engine_init() -> void;
+        void enqueue(goblin_engineer::message, actor_zeta::execution_device*) override;
 
         auto start() -> void;
 
+        auto init() -> void;
+
+    private:
+
+        auto jupyter_engine_init() -> void;
+
+        auto jupyter_kernel_init() -> void;
+
         boost::filesystem::path jupyter_connection_path;
         boost::filesystem::path script_path;
-        sandbox_mode mode;
+        sandbox_mode_t mode_;
         py::scoped_interpreter python_;
         py::module pyrocketjoe;
         std::unique_ptr<python_sandbox::detail::file_manager> file_manager_;
@@ -56,8 +58,8 @@ namespace rocketjoe { namespace services {
         std::vector<zmq::pollitem_t> jupyter_kernel_commands_polls;
         std::vector<zmq::pollitem_t> jupyter_kernel_infos_polls;
         boost::intrusive_ptr<pykernel> jupyter_kernel;
-        std::unique_ptr<std::thread> commands_exuctor;  ///TODO: HACK
-        std::unique_ptr<std::thread> infos_exuctor;  ///TODO: HACK
+        std::unique_ptr<std::thread> commands_exuctor; ///TODO: HACK
+        std::unique_ptr<std::thread> infos_exuctor;    ///TODO: HACK
     };
 
-}}
+}} // namespace rocketjoe::services
