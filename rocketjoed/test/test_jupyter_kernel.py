b1f5fdb52ed627125d032892cf2fc35c0f00d70e
Sergey Sherkunov <leinlawun@leinlawun.org>
2020-05-18 15:59:41 +0300

    Add benches for ipykernel and RocketJoe, fix detecting ipykernel

--- rocketjoed/test/test_jupyter_kernel.py
+++ rocketjoed/test/test_jupyter_kernel.py
@@ -1,4 +1,9 @@
+# Don't import KernelTests
 import jupyter_kernel_test
+import jupyter_kernel_mgmt
+from jupyter_kernel_mgmt.discovery import KernelFinder
+from jupyter_kernel_mgmt.client import BlockingKernelClient
+
 
 class RocketJoePythonTests(jupyter_kernel_test.KernelTests):
     kernel_name = 'jupyter'
@@ -9,8 +14,8 @@ class RocketJoePythonTests(jupyter_kernel_test.KernelTests):
     completion_samples = [{
         'text': 'zi', 'matches': {'zip'},
     }]
-    complete_code_samples = ['1', 'print(\'hello, world\')', 'def f(x):\n  return x * 2']
-    incomplete_code_samples = ['print(\'\'\'hello', 'def f(x):\n  x * 2']
+    complete_code_samples = ['1', 'print(\'hello, world\')', 'def f(x):\n  return x * 2\n']
+    incomplete_code_samples = ['print(\'\'\'hello', 'def f(x):\n  x*2']
     invalid_code_samples = ['import = 7q']
     #code_page_something = 'zip?'
     code_generate_error = 'raise'
@@ -27,3 +32,101 @@ class RocketJoePythonTests(jupyter_kernel_test.KernelTests):
     code_history_pattern = '1?2*'
     #supported_history_operations = ('tail', 'search')
     code_inspect_sample = 'zip'
+
+
+def kernel_info_request(kc) -> None:
+    kc.kernel_info()
+
+
+def execute_request_1(kc) -> None:
+    kc.execute_interactive("i = 0\ni + 1")
+
+
+def execute_request_2(kc) -> None:
+    kc.execute_interactive("print(6 * 7)")
+
+
+def complete_request_1(kc) -> None:
+    kc.complete("prin")
+
+
+def complete_request_2(kc) -> None:
+    kc.complete("print")
+
+
+def is_complete_request_1(kc) -> None:
+    kc.is_complete("prin")
+
+
+def is_complete_request_2(kc) -> None:
+    kc.is_complete("print")
+
+
+def test_ipykernel_kernel_info_request(benchmark) -> None:
+    with jupyter_kernel_mgmt.run_kernel_blocking('spec/python3') as kc:
+        benchmark(kernel_info_request, kc)
+
+
+def test_jupyter_kernel_info_request(benchmark) -> None:
+    with jupyter_kernel_mgmt.run_kernel_blocking('spec/jupyter') as kc:
+        benchmark(kernel_info_request, kc)
+
+
+def test_ipykernel_execute_request_1(benchmark) -> None:
+    with jupyter_kernel_mgmt.run_kernel_blocking('spec/python3') as kc:
+        benchmark(execute_request_1, kc)
+
+
+def test_jupyter_execute_request_1(benchmark) -> None:
+    with jupyter_kernel_mgmt.run_kernel_blocking('spec/jupyter') as kc:
+        benchmark(execute_request_1, kc)
+
+
+def test_ipykernel_execute_request_2(benchmark) -> None:
+    with jupyter_kernel_mgmt.run_kernel_blocking('spec/python3') as kc:
+        benchmark(execute_request_2, kc)
+
+
+def test_jupyter_execute_request_2(benchmark) -> None:
+    with jupyter_kernel_mgmt.run_kernel_blocking('spec/jupyter') as kc:
+        benchmark(execute_request_2, kc)
+
+
+def test_ipykernel_complete_request_1(benchmark) -> None:
+    with jupyter_kernel_mgmt.run_kernel_blocking('spec/python3') as kc:
+        benchmark(complete_request_1, kc)
+
+
+def test_jupyter_complete_request_1(benchmark) -> None:
+    with jupyter_kernel_mgmt.run_kernel_blocking('spec/jupyter') as kc:
+        benchmark(complete_request_1, kc)
+
+
+def test_ipykernel_complete_request_2(benchmark) -> None:
+    with jupyter_kernel_mgmt.run_kernel_blocking('spec/python3') as kc:
+        benchmark(complete_request_2, kc)
+
+
+def test_jupyter_complete_request_2(benchmark) -> None:
+    with jupyter_kernel_mgmt.run_kernel_blocking('spec/jupyter') as kc:
+        benchmark(complete_request_2, kc)
+
+
+def test_ipykernel_is_complete_request_1(benchmark) -> None:
+    with jupyter_kernel_mgmt.run_kernel_blocking('spec/python3') as kc:
+        benchmark(is_complete_request_1, kc)
+
+
+def test_jupyter_is_complete_request_1(benchmark) -> None:
+    with jupyter_kernel_mgmt.run_kernel_blocking('spec/jupyter') as kc:
+        benchmark(is_complete_request_1, kc)
+
+
+def test_ipykernel_is_complete_request_2(benchmark) -> None:
+    with jupyter_kernel_mgmt.run_kernel_blocking('spec/python3') as kc:
+        benchmark(is_complete_request_2, kc)
+
+
+def test_jupyter_is_complete_request_2(benchmark) -> None:
+    with jupyter_kernel_mgmt.run_kernel_blocking('spec/jupyter') as kc:
+        benchmark(is_complete_request_2, kc)
