bb093259e8aaa0355dde2f39ffb8304bc0263181
kotbegemot <k0tb9g9m0t@yandex.ru>
2020-05-23 00:59:02 +0300

    update

---
+++ core/rocketjoe/protocol/tests/main.cpp
@@ -0,0 +1,19 @@
+#include "gtest/gtest.h"
+
+#include <flatbuffers/idl.h>
+#include <flatbuffers/util.h>
+#include <flatbuffers/minireflect.h>
+#include <flatbuffers/flatbuffers.h>
+#include <flatbuffers/flexbuffers.h>
+
+
+#include "message_generated.h"
+
+TEST (SquareRootTest, PositiveNos) {
+    flatbuffers::Parser p;
+
+}
+
+TEST (SquareRootTest, ZeroAndNegativeNos) {
+
+}
