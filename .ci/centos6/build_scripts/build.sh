cc0b9aa42e5db687ee1885e4defaf3783d0ba258
kotbegemot <k0tb9g9m0t@yandex.ru>
2020-06-22 12:15:18 +0300

    chnage

---
+++ .ci/centos6/build_scripts/build.sh
@@ -0,0 +1,178 @@
+#!/bin/bash
+# Top-level build script called from Dockerfile
+
+# Stop at any error, show all commands
+set -ex
+
+# Set build environment variables
+MY_DIR=$(dirname "${BASH_SOURCE[0]}")
+. $MY_DIR/build_env.sh
+
+# Dependencies for compiling Python that we want to remove from
+# the final image after compiling Python
+PYTHON_COMPILE_DEPS="zlib-devel bzip2-devel expat-devel ncurses-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel"
+
+MANYLINUX2010_DEPS="glibc-devel libstdc++-devel glib2-devel libX11-devel libXext-devel libXrender-devel mesa-libGL-devel libICE-devel libSM-devel"
+
+# Get build utilities
+source $MY_DIR/build_utils.sh
+
+# Prerequisite for architecture
+case $AUDITWHEEL_ARCH in
+x86_64)
+  # See https://unix.stackexchange.com/questions/41784/can-yum-express-a-preference-for-x86-64-over-i386-packages
+  echo "multilib_policy=best" >> /etc/yum.conf
+  ;;
+esac
+
+yum -y update
+
+# EPEL support (for cmake28 & yasm)
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm curl
+
+case $AUDITWHEEL_ARCH in
+x86_64)
+  # Install centos-release-scl to get devtoolset-8
+  yum -y install centos-release-scl
+  ;;
+i686)
+  # Add libgfortran4 for devtoolset-7 compat
+  yum -y install libgfortran4
+  # Install mayeut/devtoolset-8 repo to get devtoolset-8
+  curl -fsSLo /etc/yum.repos.d/mayeut-devtoolset-8.repo https://copr.fedorainfracloud.org/coprs/mayeut/devtoolset-8/repo/epel-6/mayeut-devtoolset-8-epel-6.repo
+  ;;
+esac
+
+# Development tools and libraries
+yum -y install \
+    automake \
+    bison \
+    bzip2 \
+    devtoolset-8-binutils \
+    devtoolset-8-gcc \
+    devtoolset-8-gcc-c++ \
+    devtoolset-8-gcc-gfortran \
+    diffutils \
+    gettext \
+    file \
+    kernel-devel-`uname -r` \
+    libffi-devel \
+    make \
+    patch \
+    unzip \
+    which \
+    yasm \
+    ${PYTHON_COMPILE_DEPS}
+
+# Install a git we link against system OpenSSL/Curl
+yum -y install openssl-devel keyutils-libs-devel krb5-devel libcom_err-devel libidn-devel curl-devel perl-devel
+build_git $GIT_ROOT $GIT_HASH
+git version
+yum -y erase openssl-devel keyutils-libs-devel krb5-devel libcom_err-devel libidn-devel curl-devel perl-devel
+
+# Build an OpenSSL for Pythons. We'll delete this at the end.
+build_openssl $OPENSSL_ROOT $OPENSSL_HASH
+
+# Install newest autoconf
+build_autoconf $AUTOCONF_ROOT $AUTOCONF_HASH
+autoconf --version
+
+# Install newest automake
+build_automake $AUTOMAKE_ROOT $AUTOMAKE_HASH
+automake --version
+
+# Install newest libtool
+build_libtool $LIBTOOL_ROOT $LIBTOOL_HASH
+libtool --version
+
+# Install a more recent SQLite3
+curl -fsSLO $SQLITE_AUTOCONF_DOWNLOAD_URL/$SQLITE_AUTOCONF_VERSION.tar.gz
+check_sha256sum $SQLITE_AUTOCONF_VERSION.tar.gz $SQLITE_AUTOCONF_HASH
+tar xfz $SQLITE_AUTOCONF_VERSION.tar.gz
+cd $SQLITE_AUTOCONF_VERSION
+do_standard_install
+cd ..
+rm -rf $SQLITE_AUTOCONF_VERSION*
+rm /usr/local/lib/libsqlite3.a
+
+# Install libcrypt.so.1 and libcrypt.so.2
+build_libxcrypt "$LIBXCRYPT_DOWNLOAD_URL" "$LIBXCRYPT_VERSION" "$LIBXCRYPT_HASH"
+
+# Compile the latest Python releases.
+# (In order to have a proper SSL module, Python is compiled
+# against a recent openssl [see env vars above], which is linked
+# statically.
+mkdir -p /opt/python
+build_cpythons $CPYTHON_VERSIONS
+
+PY37_BIN=/opt/python/cp37-cp37m/bin
+
+# Install certifi and auditwheel
+$PY37_BIN/pip install --require-hashes -r $MY_DIR/py37-requirements.txt
+
+# Our openssl doesn't know how to find the system CA trust store
+#   (https://github.com/pypa/manylinux/issues/53)
+# And it's not clear how up-to-date that is anyway
+# So let's just use the same one pip and everyone uses
+ln -s $($PY37_BIN/python -c 'import certifi; print(certifi.where())') \
+      /opt/_internal/certs.pem
+# If you modify this line you also have to modify the versions in the
+# Dockerfiles:
+export SSL_CERT_FILE=/opt/_internal/certs.pem
+
+# Now we can delete our built OpenSSL headers/static libs since we've linked everything we need
+rm -rf /usr/local/ssl
+
+# Install patchelf (latest with unreleased bug fixes) and apply our patches
+build_patchelf $PATCHELF_VERSION $PATCHELF_HASH
+
+ln -s $PY37_BIN/auditwheel /usr/local/bin/auditwheel
+
+# Clean up development headers and other unnecessary stuff for
+# final image
+yum -y erase \
+    avahi \
+    bitstream-vera-fonts \
+    freetype \
+    gettext \
+    gtk2 \
+    hicolor-icon-theme \
+    libX11 \
+    wireless-tools \
+    ${PYTHON_COMPILE_DEPS} > /dev/null 2>&1
+yum -y install ${MANYLINUX2010_DEPS}
+yum -y clean all > /dev/null 2>&1
+yum list installed
+
+# we don't need libpython*.a, and they're many megabytes
+find /opt/_internal -name '*.a' -print0 | xargs -0 rm -f
+
+# Strip what we can -- and ignore errors, because this just attempts to strip
+# *everything*, including non-ELF files:
+find /opt/_internal -type f -print0 \
+    | xargs -0 -n1 strip --strip-unneeded 2>/dev/null || true
+find /usr/local -type f -print0 \
+    | xargs -0 -n1 strip --strip-unneeded 2>/dev/null || true
+
+
+# We do not need the Python test suites, or indeed the precompiled .pyc and
+# .pyo files. Partially cribbed from:
+#    https://github.com/docker-library/python/blob/master/3.4/slim/Dockerfile
+find /opt/_internal -depth \
+     \( -type d -a -name test -o -name tests \) \
+  -o \( -type f -a -name '*.pyc' -o -name '*.pyo' \) | xargs rm -rf
+
+# Fix libc headers to remain compatible with C99 compilers.
+find /usr/include/ -type f -exec sed -i 's/\bextern _*inline_*\b/extern __inline __attribute__ ((__gnu_inline__))/g' {} +
+
+# remove useless things that have been installed by devtoolset
+rm -rf $DEVTOOLSET_ROOTPATH/usr/share/man
+find $DEVTOOLSET_ROOTPATH/usr/share/locale -mindepth 1 -maxdepth 1 -not \( -name 'en*' -or -name 'locale.alias' \) | xargs rm -rf
+rm -rf /usr/share/backgrounds
+# if we updated glibc, we need to strip locales again...
+localedef --list-archive | grep -v -i ^en_US.utf8 | xargs localedef --delete-from-archive
+mv -f /usr/lib/locale/locale-archive /usr/lib/locale/locale-archive.tmpl
+build-locale-archive
+find /usr/share/locale -mindepth 1 -maxdepth 1 -not \( -name 'en*' -or -name 'locale.alias' \) | xargs rm -rf
+find /usr/local/share/locale -mindepth 1 -maxdepth 1 -not \( -name 'en*' -or -name 'locale.alias' \) | xargs rm -rf
+rm -rf /usr/local/share/man
---
+++ .ci/centos6/build_scripts/build_env.sh
@@ -0,0 +1,38 @@
+# source me
+
+PYTHON_DOWNLOAD_URL=https://www.python.org/ftp/python
+# of the form <maj>.<min>.<rev> or <maj>.<min>.<rev>rc<n>
+CPYTHON_VERSIONS="3.7.7"
+
+# openssl version to build, with expected sha256 hash of .tar.gz
+# archive.
+OPENSSL_ROOT=openssl-1.1.1g
+OPENSSL_HASH=ddb04774f1e32f0c49751e21b67216ac87852ceb056b75209af2443400636d46
+OPENSSL_DOWNLOAD_URL=https://www.openssl.org/source
+
+PATCHELF_VERSION=0.11
+PATCHELF_HASH=e9dc4dbed842e475176ef60531c2805ed37a71c34cc6dc5d1b9ad68d889aeb6b
+
+AUTOCONF_ROOT=autoconf-2.69
+AUTOCONF_HASH=954bd69b391edc12d6a4a51a2dd1476543da5c6bbf05a95b59dc0dd6fd4c2969
+AUTOCONF_DOWNLOAD_URL=http://ftp.gnu.org/gnu/autoconf
+
+AUTOMAKE_ROOT=automake-1.16.2
+AUTOMAKE_HASH=b2f361094b410b4acbf4efba7337bdb786335ca09eb2518635a09fb7319ca5c1
+AUTOMAKE_DOWNLOAD_URL=http://ftp.gnu.org/gnu/automake
+
+LIBTOOL_ROOT=libtool-2.4.6
+LIBTOOL_HASH=e3bd4d5d3d025a36c21dd6af7ea818a2afcd4dfc1ea5a17b39d7854bcd0c06e3
+LIBTOOL_DOWNLOAD_URL=http://ftp.gnu.org/gnu/libtool
+
+SQLITE_AUTOCONF_VERSION=sqlite-autoconf-3310100
+SQLITE_AUTOCONF_HASH=62284efebc05a76f909c580ffa5c008a7d22a1287285d68b7825a2b6b51949ae
+SQLITE_AUTOCONF_DOWNLOAD_URL=https://www.sqlite.org/2020
+
+LIBXCRYPT_VERSION=4.4.16
+LIBXCRYPT_DOWNLOAD_URL=https://codeload.github.com/besser82/libxcrypt/tar.gz
+LIBXCRYPT_HASH=a98f65b8baffa2b5ba68ee53c10c0a328166ef4116bce3baece190c8ce01f375
+
+GIT_ROOT=2.26.2
+GIT_HASH=21b48d1e7e03d0b325967e31a30f4fa878e4fbb6dac6e997b5dc3f41f950a6d3
+GIT_DOWNLOAD_URL=https://github.com/git/git/archive
