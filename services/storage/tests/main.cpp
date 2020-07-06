7e9a3ce061541db277ab74795a258f14260defcd
kotbegemot <k0tb9g9m0t@yandex.ru>
2020-07-06 22:59:42 +0300

    test fix

--- services/storage/tests/main.cpp
+++ services/storage/tests/main.cpp
@@ -1,20 +1,22 @@
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
+
+#include <string>
+
 #include <services/storage/hash_storage.hpp>
 #include <services/storage/storage_engine.hpp>
 
 constexpr static char* name = "test_name";
 
-TEST(register_storage_storage, storage_engine) {
-    services::storage_engine se;
-    se.register_storage(name, std::make_unique<services::memory_hash_storage>());
-    auto& storage_type = se.get_store(name);
-}
-
 TEST(init, memory_hash_storage) {
     auto storage = std::make_unique<services::memory_hash_storage>();
-    services::temporary_buffer_storage tbs;
-    tbs.emplace_back("1111111111", std::make_unique<services::buffer_tt>("111111111"));
-    storage->put(tbs);
+    services::temporary_buffer_storage tbs_input;
+    tbs_input.emplace_back("1", std::make_unique<services::buffer_tt>("2"));
+    storage->put(tbs_input);
+    services::temporary_buffer_storage tbs_out;
+    tbs_out.emplace_back("1", std::make_unique<services::buffer_tt>());
+    storage->get(tbs_out);
+    auto element = (*(tbs_out.at(0).second));
+    ASSERT_EQ(element,std::string("2"));
 }
 
 int main(int argc, char** argv) {
