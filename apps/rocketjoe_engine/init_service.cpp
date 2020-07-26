0f48374b39e92456bd2c22470ee58244dcf48a08
kotbegemot <k0tb9g9m0t@yandex.ru>
2020-07-26 15:04:59 +0300

    fix

--- apps/rocketjoe_engine/init_service.cpp
+++ apps/rocketjoe_engine/init_service.cpp
@@ -2,6 +2,7 @@
 
 #include <goblin-engineer/components/root_manager.hpp>
 #include <services/interactive_python_interpreter/interactive_python.hpp>
+#include <services/jupyter/jupyter.hpp>
 
 using goblin_engineer::components::make_manager_service;
 using goblin_engineer::components::make_service;
@@ -9,10 +10,7 @@ using goblin_engineer::components::root_manager;
 using namespace goblin_engineer::components;
 
 void init_service(goblin_engineer::components::root_manager& env, components::configuration& cfg, components::log_t& log) {
-    if (cfg.python_configuration_.mode_ != components::sandbox_mode_t::script) {
-        auto python = make_manager_service<services::interactive_python>(env, cfg.python_configuration_, log);
-        python->init();
-        python->start();
-    }
+    auto jupyter = make_manager_service<services::jupyter>(env, cfg.python_configuration_, log);
+    make_service<services::interactive_python>(jupyter,cfg.python_configuration_, log);
 
 }
