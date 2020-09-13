7698fead2c5e053d5e74681f71001e98ecd51de2
Alexander <k0tb9g9m0t@yandex.ru>
2020-09-13 15:31:44 +0300

    All projects are build with an address_sanitizer(#115)

--- cmake/address_sanitizer.cmake
+++ cmake/address_sanitizer.cmake
@@ -1,6 +1,2 @@
-option(ADDRESS_SANITIZER "Enable Clang AddressSanitizer" OFF)
-
-if (ADDRESS_SANITIZER)
-    message(STATUS "AddressSanitizer enabled for build")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O1 -fno-omit-frame-pointer -fsanitize=address")
-endif ()
+message(STATUS "AddressSanitizer enabled for build")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O1 -fno-omit-frame-pointer -fsanitize=address")
