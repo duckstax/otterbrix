66fea7c1a33bf3857aa41368d8590c419fdad2da
makeyavelly <nikita.makeenkov@gmail.com>
2021-09-19 14:48:14 +0300

    test

--- components/storage/test/main.cpp
+++ components/storage/test/main.cpp
@@ -1,17 +1,3 @@
-#include <cstdint>
-#include <iostream>
-#include <memory>
-#include <any>
-
-int main() {
-    std::cerr << sizeof(std::uint64_t) << std::endl;
-    std::cerr << sizeof(std::int64_t) << std::endl;
-    std::cerr << sizeof(double) << std::endl;
-    std::cerr << sizeof(float) << std::endl;
-    std::cerr << sizeof(void*) << std::endl;
-    std::cerr << sizeof(std::unique_ptr<std::string>) << std::endl;
-    std::cerr << sizeof(std::any) << std::endl;
-    std::cerr << sizeof(std::pair<int,int>) << std::endl;
-    std::cerr << sizeof(std::pair<std::unique_ptr<std::string>,std::unique_ptr<std::string>>) << std::endl;
-
-}
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
