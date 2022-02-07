5472d5b3880738df4622a87aec6af7bc14573442
Alexander <k0tb9g9m0t@yandex.ru>
2022-02-07 22:57:34 +0300

    remove process pool (#151)

--- tests/client.py
+++
@@ -1,8 +0,0 @@
-from tasks import *
-subprocess_task.delay()
-create_audio_wave_task.delay()
-async_task.delay()
-request_task.delay()
-yield_task.delay()
-async_yield_task.delay()
-start_many_tasks.delay()
