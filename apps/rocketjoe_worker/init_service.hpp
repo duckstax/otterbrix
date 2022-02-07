1e12cf5758a769bf0e9e5ec5658ddd2b282184c8
kotbegemot <k0tb9g9m0t@yandex.ru>
2022-02-07 11:36:42 +0300

    remove-process-pool

--- apps/rocketjoe_worker/init_service.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <components/configuration/configuration.hpp>
-#include <components/log/log.hpp>
-#include <goblin-engineer/components/root_manager.hpp>
-
-void init_service(goblin_engineer::components::root_manager& env, components::configuration& cfg, components::log_t& log);
