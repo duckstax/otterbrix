3ee4952e7d7b1a8b65d42e99f1bc16183f3952a5
kotbegemot <k0tb9g9m0t@yandex.ru>
2022-02-07 11:42:00 +0300

    remove old example

--- example/args_test.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-import sys
-
-print('Number of arguments:', len(sys.argv), 'arguments.')
-print('Argument List:', str(sys.argv))
--- example/basic.py
+++
@@ -1,6 +0,0 @@
-from pyrocketjoe import file_read
-
-file = file_read('big_data.txt')
-
-for i in file.values():
-    print(i)
