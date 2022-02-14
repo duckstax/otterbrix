67ce05d036ad6820d28d7b4ba7bb4590e07da7da
makeyavelly <nikita.makeenkov@gmail.com>
2022-02-14 22:42:26 +0300

    base b-tree storage (#153)

---
+++ components/document/structure.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <components/document/core/value.hpp>
+#include <components/document/core/array.hpp>
+#include <components/document/mutable/mutable_array.h>
+
+namespace components::document::structure {
+
+    enum class attribute : uint32_t {
+        type = 0,
+        offset = 1,
+        size = 2,
+        version = 3
+    };
+
+    uint32_t index_attribute(attribute attr);
+    bool is_attribute(const ::document::impl::value_t *field, attribute attr);
+    const ::document::impl::value_t *get_attribute(const ::document::impl::value_t *field, attribute attr);
+
+    template <class T>
+    void set_attribute(const ::document::impl::value_t *field, attribute attr, T value) {
+        field->as_array()->as_mutable()->set(index_attribute(attr), value);
+    }
+
+}
