03c1760415881162b322d7ddd1677875eb36bfba
kotbegemot <k0tb9g9m0t@yandex.ru>
2022-09-13 23:47:35 +0300

    fix build 8 test

---
+++ core/btree/btree.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <memory_resource>
+
+#include "absl/container/btree_map.h"
+
+namespace core::btree {
+
+    template<
+        typename Key,
+        typename Value,
+        typename Compare = std::less<Key>,
+        typename Alloc = std::allocator<std::pair<const Key, Value>>>
+    using btree_t = absl::btree_map<Key, Value, Compare, Alloc>;
+
+} // namespace core::btree
+
+namespace core::pmr::btree {
+    template<
+        typename Key,
+        typename Value,
+        typename Compare = std::less<Key>>
+    using btree_t = absl::btree_map<Key, Value, Compare, std::pmr::polymorphic_allocator<std::pair<const Key, Value>>>;
+}
