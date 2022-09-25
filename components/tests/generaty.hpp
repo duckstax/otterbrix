81b79efefef22927e722886889efa21055aa65c5
makeyavelly <nikita.makeenkov@gmail.com>
2022-09-25 22:05:35 +0300

    repair build document

--- components/tests/generaty.hpp
+++ components/tests/generaty.hpp
@@ -1,11 +1,10 @@
 #pragma once
 
-#include <components/serialize/serialize.hpp>
+#include <components/document/document.hpp>
 #include <components/document/mutable/mutable_array.h>
 #include <components/document/mutable/mutable_dict.h>
 
 using namespace components::document;
-using namespace components::serialize;
 
 document::retained_t<document::impl::array_t> gen_array(int num);
 document::retained_t<document::impl::dict_t> gen_dict(int num);
