049e1c0f36552c198934eee0efe549eefb5d6eae
makeyavelly <nikita.makeenkov@gmail.com>
2022-10-02 23:09:43 +0300

    operator $avg

--- services/collection/tests/operators/test_aggregate_operators.cpp
+++ services/collection/tests/operators/test_aggregate_operators.cpp
@@ -4,6 +4,7 @@
 #include <services/collection/operators/aggregate/operator_min.hpp>
 #include <services/collection/operators/aggregate/operator_max.hpp>
 #include <services/collection/operators/aggregate/operator_sum.hpp>
+#include <services/collection/operators/aggregate/operator_avg.hpp>
 #include "test_operator_generaty.hpp"
 
 using namespace services::collection::operators;
@@ -102,5 +103,29 @@ TEST_CASE("operator::aggregate::sum") {
                                                       predicates::limit_t::unlimit()));
         sum_.on_execute(nullptr);
         REQUIRE(sum_.value()->as_unsigned() == 45);
+    }
+}
+
+TEST_CASE("operator::aggregate::avg") {
+    auto collection = init_collection();
+
+    SECTION("avg::all") {
+        auto cond = parse_find_condition("{}");
+        operator_avg_t avg_(d(collection)->view(), "count");
+        avg_.set_children(std::make_unique<full_scan>(d(collection)->view(),
+                                                      predicates::create_predicate(d(collection)->view(), cond),
+                                                      predicates::limit_t::unlimit()));
+        avg_.on_execute(nullptr);
+        REQUIRE(avg_.value()->as_double() == 50.5);
+    }
+
+    SECTION("avg::match") {
+        auto cond = parse_find_condition(R"({"count": {"$lt": 10}})");
+        operator_avg_t avg_(d(collection)->view(), "count");
+        avg_.set_children(std::make_unique<full_scan>(d(collection)->view(),
+                                                      predicates::create_predicate(d(collection)->view(), cond),
+                                                      predicates::limit_t::unlimit()));
+        avg_.on_execute(nullptr);
+        REQUIRE(avg_.value()->as_double() == 5.0);
     }
 }
