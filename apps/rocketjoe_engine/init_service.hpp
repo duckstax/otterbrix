c7549946a0635f3681863206a722026bc1eb1e1e
kotbegemot <k0tb9g9m0t@yandex.ru>
2022-10-19 16:58:26 +0300

    clear

--- apps/rocketjoe_engine/init_service.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <components/configuration/configuration.hpp>
-#include <components/log/log.hpp>
-#include <goblin-engineer/components/root_manager.hpp>
-#include <services/jupyter/jupyter.hpp>
-
-void init_service(actor_zeta::intrusive_ptr<services::jupyter>, components::configuration& , components::log_t& );
