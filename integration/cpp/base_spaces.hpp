eadb0dd0ebc348c88d773f0a81900e1d7df2ffc7
kotbegemot <k0tb9g9m0t@yandex.ru>
2022-10-27 10:13:13 +0300

    init

---
+++ integration/cpp/base_spaces.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <components/log/log.hpp>
+#include <components/configuration/configuration.hpp>
+
+#include <services/dispatcher/dispatcher.hpp>
+#include <services/wal/manager_wal_replicate.hpp>
+#include <services/disk/manager_disk.hpp>
+#include <services/database/database.hpp>
+
+#include "wrapper_dispatcher.hpp"
+
+#include <core/excutor.hpp>
+
+namespace duck_charmer {
+
+    class base_spaces {
+    public:
+        base_spaces(base_spaces& other) = delete;
+        void operator=(const base_spaces&) = delete;
+
+        log_t& get_log();
+        duck_charmer::wrapper_dispatcher_t* dispatcher();
+
+        ~base_spaces();
+
+    protected:
+        explicit base_spaces(const configuration::config& config);
+
+        log_t log_;
+        actor_zeta::scheduler_ptr  scheduler_;
+        actor_zeta::detail::pmr::memory_resource* resource;
+        services::database::manager_database_ptr manager_database_;
+        services::dispatcher::manager_dispatcher_ptr manager_dispatcher_;
+        services::wal::manager_wal_ptr manager_wal_;
+        services::disk::manager_disk_ptr manager_disk_;
+        std::unique_ptr<duck_charmer::wrapper_dispatcher_t> wrapper_dispatcher_;
+    };
+
+} // namespace python
