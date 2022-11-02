c2e68c7c88841f640378487e5f075692e6c15b6a
kotbegemot <k0tb9g9m0t@yandex.ru>
2022-11-03 00:34:22 +0300

    fix build

---
+++ integration/python/ottergon/setup.py
@@ -0,0 +1,17 @@
+
+from skbuild import setup
+
+setup(
+    name="ottergon",
+    version="1.0.0",
+    description=" ",
+    author=" ",
+    license=" ",
+    packages=['ottergon'],
+    #package_dir={'': 'integration/python'},
+    #package_data={"": ["libiwasm.so"]},
+    #cmake_install_dir='integration/python',
+    python_requires='>=3.6',
+    #cmake_source_dir=".",
+    include_package_data=True
+)
