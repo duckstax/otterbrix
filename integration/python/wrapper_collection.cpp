45ff0e9cfb4d449a6b0f5863d474c6ba20aa0e68
kotbegemot <k0tb9g9m0t@yandex.ru>
2022-11-04 12:27:22 +0300

    fix build

--- integration/python/wrapper_collection.cpp
+++ integration/python/wrapper_collection.cpp
@@ -6,7 +6,7 @@
 #include <pybind11/stl.h>
 #include <pybind11/stl_bind.h>
 #include <services/collection/result.hpp>
-#include <wrapper_database.hpp>
+#include "wrapper_database.hpp"
 
 // The bug related to the use of RTTI by the pybind11 library has been fixed: a
 // declaration should be in each translation unit.
