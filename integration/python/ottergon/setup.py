dc4b9265ea35dee294b7ce03b511a08c97507bae
kotbegemot <k0tb9g9m0t@yandex.ru>
2022-11-04 14:41:45 +0300

    fix build

--- integration/python/ottergon/setup.py
+++ integration/python/ottergon/setup.py
@@ -1,5 +1,11 @@
-
+import subprocess
 from skbuild import setup
+
+import skbuild.constants
+
+build_dir = skbuild.constants.CMAKE_BUILD_DIR()
+
+subprocess.run([f"conan install . -s build_type=Release -if={build_dir}"], shell=True, check=True)
 
 setup(
     name="ottergon",
@@ -8,10 +14,10 @@ setup(
     author=" ",
     license=" ",
     packages=['ottergon'],
-    #package_dir={'': 'integration/python'},
-    #package_data={"": ["libiwasm.so"]},
-    #cmake_install_dir='integration/python',
+    # package_dir={'': 'integration/python'},
+    # package_data={"": ["libiwasm.so"]},
+    # cmake_install_dir='integration/python',
     python_requires='>=3.6',
-    #cmake_source_dir=".",
+    # cmake_source_dir=".",
     include_package_data=True
 )
