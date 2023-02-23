1df1ed14e3d7d7fd627253b432da8aa961cf2cea
makeyavelly <nikita.makeenkov@gmail.com>
2023-02-23 23:25:25 +0300

    benchmark into index_scan

--- integration/cpp/benchmark/classes.hpp
+++ integration/cpp/benchmark/classes.hpp
@@ -1,77 +1,125 @@
 #pragma once
 
-#include <integration/python/spaces.hpp>
-#include <benchmark/benchmark.h>
-#include <components/document/document.hpp>
-#include <components/document/mutable/mutable_array.h>
-#include <components/document/mutable/mutable_dict.h>
+#include <integration/cpp/base_spaces.hpp>
+//#include <components/document/document.hpp>
+//#include <components/document/mutable/mutable_array.h>
+//#include <components/document/mutable/mutable_dict.h>
 #include <components/tests/generaty.hpp>
 
 static const database_name_t database_name = "TestDatabase";
-static const collection_name_t collection_name = "TestCollection";
-static constexpr int size_collection = 10000;
+static const collection_name_t collection_name_without_index = "TestCollectionWithoutIndex";
+static const collection_name_t collection_name_with_index = "TestCollectionWithIndex";
+constexpr int size_collection = 10000;
 
 
-inline configuration::config create_null_config() {
+inline configuration::config create_config(bool on_disk) {
     auto config = configuration::config::default_config();
     config.log.level = log_t::level::off;
-    config.disk.on = false;
-    config.wal.sync_to_disk = false;
+    config.disk.on = on_disk;
+    config.wal.sync_to_disk = on_disk;
     return config;
 }
 
-class unique_spaces final : public duck_charmer::base_spaces {
+template <bool on_disk>
+class test_spaces final : public duck_charmer::base_spaces {
 public:
-    static unique_spaces &get() {
-        static unique_spaces spaces_;
+    static test_spaces &get() {
+        static test_spaces<on_disk> spaces_;
         return spaces_;
     }
 
 private:
-    unique_spaces()
-        : duck_charmer::base_spaces(create_null_config())
+    test_spaces()
+        : duck_charmer::base_spaces(create_config(on_disk))
     {}
 };
 
 
-void init_collection() {
-    auto* dispatcher = unique_spaces::get().dispatcher();
+template <bool on_disk>
+void init_collection(const collection_name_t& collection_name) {
+    auto* resource = actor_zeta::detail::pmr::get_default_resource();
+    auto* dispatcher = test_spaces<on_disk>::get().dispatcher();
     auto session = duck_charmer::session_id_t();
     dispatcher->create_database(session, database_name);
     dispatcher->create_collection(session, database_name, collection_name);
-    std::list<document_ptr> docs;
+    std::pmr::vector<document_ptr> docs(resource);
     for (int i = 1; i <= size_collection; ++i) {
         docs.push_back(gen_doc(i));
     }
     dispatcher->insert_many(session, database_name, collection_name, docs);
 }
 
-template<class T>
-document::retained_t<mutable_dict_t> make_dict(const std::string& field, const std::string& key, T value) {
-    auto key_value = mutable_dict_t::new_dict();
-    key_value->set(key, value);
-    auto cond = mutable_dict_t::new_dict();
-    cond->set(field, key_value);
-    return cond;
+template <bool on_disk>
+void create_index(const collection_name_t& collection_name) {
+    auto* dispatcher = test_spaces<on_disk>::get().dispatcher();
+    auto session = duck_charmer::session_id_t();
+    components::ql::create_index_t ql{database_name, collection_name, components::ql::index_type::single};
+    ql.keys_.emplace_back("count");
+    dispatcher->create_index(session, ql);
 }
 
-template<class T>
-document_ptr make_condition(const std::string& field, const std::string& key, T value) {
-    auto dict = make_dict(field, key, value);
-    return make_document(dict);
+template <bool on_disk>
+void init_spaces() {
+    init_collection<on_disk>(collection_name_without_index);
+    init_collection<on_disk>(collection_name_with_index);
+    create_index<on_disk>(collection_name_with_index);
 }
 
-inline document::retained_t<mutable_dict_t> make_dict(const std::string& aggregate, const std::list<document::retained_t<mutable_dict_t>> &sub_dict) {
-    auto dict = mutable_dict_t::new_dict();
-    auto array = mutable_array_t::new_array();
-    for (const auto& sub_cond : sub_dict) {
-        array->append(sub_cond);
+template <bool on_disk>
+duck_charmer::wrapper_dispatcher_t* wr_dispatcher() {
+    return test_spaces<on_disk>::get().dispatcher();
+}
+
+template <bool on_index>
+collection_name_t get_collection_name() {
+    if constexpr (on_index) {
+        return collection_name_with_index;
+    } else {
+        return collection_name_without_index;
     }
-    dict->set(aggregate, array);
-    return dict;
 }
 
-inline document_ptr make_condition(const std::string& aggregate, const std::list<document::retained_t<mutable_dict_t>> &sub_conditions) {
-    auto dict = make_dict(aggregate, sub_conditions);
-    return make_document(dict);
+template <typename T = int>
+components::ql::aggregate_statement_raw_ptr create_aggregate(const collection_name_t &collection_name,
+                                                             components::expressions::compare_type compare = components::expressions::compare_type::eq,
+                                                             const std::string &key = {}, T value = T()) {
+    auto* aggregate = new components::ql::aggregate_statement(database_name, collection_name);
+    components::ql::aggregate::match_t match;
+    if (!key.empty()) {
+        aggregate->add_parameter(core::parameter_id_t{1}, value);
+        match.query = components::expressions::make_compare_expression(actor_zeta::detail::pmr::get_default_resource(), compare,
+                                                                       components::expressions::key_t{key}, core::parameter_id_t{1});
+    }
+    aggregate->append(components::ql::aggregate::operator_type::match, match);
+    return aggregate;
 }
+
+//template<class T>
+//document::retained_t<mutable_dict_t> make_dict(const std::string& field, const std::string& key, T value) {
+//    auto key_value = mutable_dict_t::new_dict();
+//    key_value->set(key, value);
+//    auto cond = mutable_dict_t::new_dict();
+//    cond->set(field, key_value);
+//    return cond;
+//}
+//
+//template<class T>
+//document_ptr make_condition(const std::string& field, const std::string& key, T value) {
+//    auto dict = make_dict(field, key, value);
+//    return make_document(dict);
+//}
+//
+//inline document::retained_t<mutable_dict_t> make_dict(const std::string& aggregate, const std::list<document::retained_t<mutable_dict_t>> &sub_dict) {
+//    auto dict = mutable_dict_t::new_dict();
+//    auto array = mutable_array_t::new_array();
+//    for (const auto& sub_cond : sub_dict) {
+//        array->append(sub_cond);
+//    }
+//    dict->set(aggregate, array);
+//    return dict;
+//}
+//
+//inline document_ptr make_condition(const std::string& aggregate, const std::list<document::retained_t<mutable_dict_t>> &sub_conditions) {
+//    auto dict = make_dict(aggregate, sub_conditions);
+//    return make_document(dict);
+//}
---
+++ integration/cpp/benchmark/index/main.cpp
@@ -0,0 +1,83 @@
+#include <benchmark/benchmark.h>
+#include "../classes.hpp"
+
+//#define test_with_disk
+
+using components::expressions::compare_type;
+
+constexpr bool disk_on = true;
+constexpr bool disk_off = false;
+constexpr bool index_on = true;
+constexpr bool index_off = false;
+
+#define BENCHMARK_FUNC(FUNC, DISK_ON) \
+    BENCHMARK(FUNC<DISK_ON, index_off>)->Arg(100); \
+    BENCHMARK(FUNC<DISK_ON, index_on>)->Arg(100)
+
+template <bool on_disk, bool on_index>
+void only_find_all(benchmark::State& state) {
+    state.PauseTiming();
+    auto* dispatcher = wr_dispatcher<on_disk>();
+    collection_name_t collection_name = get_collection_name<on_index>();
+    state.ResumeTiming();
+    for (auto _ : state) {
+        for (int i = 0; i < state.range(0); ++i) {
+            auto session = duck_charmer::session_id_t();
+            dispatcher->find(session, create_aggregate(collection_name));
+        }
+    }
+}
+
+template <bool on_disk, bool on_index>
+void only_find_eq(benchmark::State& state) {
+    state.PauseTiming();
+    auto* dispatcher = wr_dispatcher<on_disk>();
+    collection_name_t collection_name = get_collection_name<on_index>();
+    state.ResumeTiming();
+    for (auto _ : state) {
+        for (int i = 0; i < state.range(0); ++i) {
+            auto session = duck_charmer::session_id_t();
+            dispatcher->find(session, create_aggregate(collection_name, compare_type::eq, "count", 115));
+        }
+    }
+}
+
+template <bool on_disk, bool on_index>
+void only_find_gt(benchmark::State& state) {
+    state.PauseTiming();
+    auto* dispatcher = wr_dispatcher<on_disk>();
+    collection_name_t collection_name = get_collection_name<on_index>();
+    state.ResumeTiming();
+    for (auto _ : state) {
+        for (int i = 0; i < state.range(0); ++i) {
+            auto session = duck_charmer::session_id_t();
+            dispatcher->find(session, create_aggregate(collection_name, compare_type::gt, "count", size_collection - 100));
+        }
+    }
+}
+
+
+
+BENCHMARK_FUNC(only_find_all, disk_off);
+BENCHMARK_FUNC(only_find_eq, disk_off);
+BENCHMARK_FUNC(only_find_gt, disk_off);
+
+#ifdef test_with_disk
+BENCHMARK_FUNC(only_find_all, disk_on);
+BENCHMARK_FUNC(only_find_eq, disk_on);
+BENCHMARK_FUNC(only_find_gt, disk_on);
+#endif
+
+int main(int argc, char** argv) {
+    ::benchmark::Initialize(&argc, argv);
+    if (::benchmark::ReportUnrecognizedArguments(argc, argv)) {
+        return 1;
+    }
+    init_spaces<disk_off>();
+#ifdef test_with_disk
+    init_spaces<disk_on>();
+#endif
+    ::benchmark::RunSpecifiedBenchmarks();
+    ::benchmark::Shutdown();
+    return 0;
+}
