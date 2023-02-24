c3e9b4180fca5b14630c7cc43c9a2a481a24a0a2
makeyavelly <nikita.makeenkov@gmail.com>
2023-02-24 23:36:37 +0300

    benchmark on|off wal

--- integration/cpp/benchmark/classes.hpp
+++ integration/cpp/benchmark/classes.hpp
@@ -6,39 +6,44 @@
 //#include <components/document/mutable/mutable_dict.h>
 #include <components/tests/generaty.hpp>
 
+using namespace components::ql;
+using namespace components::expressions;
+
 static const database_name_t database_name = "TestDatabase";
 static const collection_name_t collection_name_without_index = "TestCollectionWithoutIndex";
 static const collection_name_t collection_name_with_index = "TestCollectionWithIndex";
 constexpr int size_collection = 10000;
 
 
-inline configuration::config create_config(bool on_disk) {
+template <bool on_wal, bool on_disk>
+inline configuration::config create_config() {
     auto config = configuration::config::default_config();
     config.log.level = log_t::level::off;
     config.disk.on = on_disk;
+    config.wal.on = on_wal;
     config.wal.sync_to_disk = on_disk;
     return config;
 }
 
-template <bool on_disk>
+template <bool on_wal, bool on_disk>
 class test_spaces final : public duck_charmer::base_spaces {
 public:
     static test_spaces &get() {
-        static test_spaces<on_disk> spaces_;
+        static test_spaces<on_wal, on_disk> spaces_;
         return spaces_;
     }
 
 private:
     test_spaces()
-        : duck_charmer::base_spaces(create_config(on_disk))
+        : duck_charmer::base_spaces(create_config<on_wal, on_disk>())
     {}
 };
 
 
-template <bool on_disk>
+template <bool on_wal, bool on_disk>
 void init_collection(const collection_name_t& collection_name) {
     auto* resource = actor_zeta::detail::pmr::get_default_resource();
-    auto* dispatcher = test_spaces<on_disk>::get().dispatcher();
+    auto* dispatcher = test_spaces<on_wal, on_disk>::get().dispatcher();
     auto session = duck_charmer::session_id_t();
     dispatcher->create_database(session, database_name);
     dispatcher->create_collection(session, database_name, collection_name);
@@ -49,25 +54,25 @@ void init_collection(const collection_name_t& collection_name) {
     dispatcher->insert_many(session, database_name, collection_name, docs);
 }
 
-template <bool on_disk>
+template <bool on_wal, bool on_disk>
 void create_index(const collection_name_t& collection_name) {
-    auto* dispatcher = test_spaces<on_disk>::get().dispatcher();
+    auto* dispatcher = test_spaces<on_wal, on_disk>::get().dispatcher();
     auto session = duck_charmer::session_id_t();
-    components::ql::create_index_t ql{database_name, collection_name, components::ql::index_type::single};
+    create_index_t ql{database_name, collection_name, index_type::single};
     ql.keys_.emplace_back("count");
     dispatcher->create_index(session, ql);
 }
 
-template <bool on_disk>
+template <bool on_wal, bool on_disk>
 void init_spaces() {
-    init_collection<on_disk>(collection_name_without_index);
-    init_collection<on_disk>(collection_name_with_index);
-    create_index<on_disk>(collection_name_with_index);
+    init_collection<on_wal, on_disk>(collection_name_without_index);
+    init_collection<on_wal, on_disk>(collection_name_with_index);
+    create_index<on_wal, on_disk>(collection_name_with_index);
 }
 
-template <bool on_disk>
+template <bool on_wal, bool on_disk>
 duck_charmer::wrapper_dispatcher_t* wr_dispatcher() {
-    return test_spaces<on_disk>::get().dispatcher();
+    return test_spaces<on_wal, on_disk>::get().dispatcher();
 }
 
 template <bool on_index>
@@ -80,17 +85,16 @@ collection_name_t get_collection_name() {
 }
 
 template <typename T = int>
-components::ql::aggregate_statement_raw_ptr create_aggregate(const collection_name_t &collection_name,
-                                                             components::expressions::compare_type compare = components::expressions::compare_type::eq,
-                                                             const std::string &key = {}, T value = T()) {
-    auto* aggregate = new components::ql::aggregate_statement(database_name, collection_name);
-    components::ql::aggregate::match_t match;
+aggregate_statement_raw_ptr create_aggregate(const collection_name_t& collection_name, compare_type compare = compare_type::eq,
+                                             const std::string& key = {}, T value = T()) {
+    auto* aggregate = new aggregate_statement(database_name, collection_name);
+    aggregate::match_t match;
     if (!key.empty()) {
         aggregate->add_parameter(core::parameter_id_t{1}, value);
-        match.query = components::expressions::make_compare_expression(actor_zeta::detail::pmr::get_default_resource(), compare,
-                                                                       components::expressions::key_t{key}, core::parameter_id_t{1});
+        match.query = make_compare_expression(actor_zeta::detail::pmr::get_default_resource(), compare,
+                                              components::expressions::key_t{key}, core::parameter_id_t{1});
     }
-    aggregate->append(components::ql::aggregate::operator_type::match, match);
+    aggregate->append(aggregate::operator_type::match, match);
     return aggregate;
 }
 
--- integration/cpp/benchmark/index/main.cpp
+++ integration/cpp/benchmark/index/main.cpp
@@ -5,19 +5,21 @@
 
 using components::expressions::compare_type;
 
+constexpr bool wal_on = true;
+constexpr bool wal_off = false;
 constexpr bool disk_on = true;
 constexpr bool disk_off = false;
 constexpr bool index_on = true;
 constexpr bool index_off = false;
 
-#define BENCHMARK_FUNC(FUNC, DISK_ON) \
-    BENCHMARK(FUNC<DISK_ON, index_off>)->Arg(100); \
-    BENCHMARK(FUNC<DISK_ON, index_on>)->Arg(100)
+#define BENCHMARK_FUNC(FUNC, WAL_ON, DISK_ON) \
+    BENCHMARK(FUNC<WAL_ON, DISK_ON, index_off>)->Arg(100); \
+    BENCHMARK(FUNC<WAL_ON, DISK_ON, index_on>)->Arg(100)
 
-template <bool on_disk, bool on_index>
+template <bool on_wal, bool on_disk, bool on_index>
 void only_find_all(benchmark::State& state) {
     state.PauseTiming();
-    auto* dispatcher = wr_dispatcher<on_disk>();
+    auto* dispatcher = wr_dispatcher<on_wal, on_disk>();
     collection_name_t collection_name = get_collection_name<on_index>();
     auto session = duck_charmer::session_id_t();
     state.ResumeTiming();
@@ -28,10 +30,10 @@ void only_find_all(benchmark::State& state) {
     }
 }
 
-template <bool on_disk, bool on_index>
+template <bool on_wal, bool on_disk, bool on_index>
 void only_find_eq(benchmark::State& state) {
     state.PauseTiming();
-    auto* dispatcher = wr_dispatcher<on_disk>();
+    auto* dispatcher = wr_dispatcher<on_wal, on_disk>();
     collection_name_t collection_name = get_collection_name<on_index>();
     auto session = duck_charmer::session_id_t();
     state.ResumeTiming();
@@ -42,10 +44,10 @@ void only_find_eq(benchmark::State& state) {
     }
 }
 
-template <bool on_disk, bool on_index>
+template <bool on_wal, bool on_disk, bool on_index>
 void only_find_gt(benchmark::State& state) {
     state.PauseTiming();
-    auto* dispatcher = wr_dispatcher<on_disk>();
+    auto* dispatcher = wr_dispatcher<on_wal, on_disk>();
     collection_name_t collection_name = get_collection_name<on_index>();
     auto session = duck_charmer::session_id_t();
     state.ResumeTiming();
@@ -58,14 +60,14 @@ void only_find_gt(benchmark::State& state) {
 
 
 
-BENCHMARK_FUNC(only_find_all, disk_off);
-BENCHMARK_FUNC(only_find_eq, disk_off);
-BENCHMARK_FUNC(only_find_gt, disk_off);
+BENCHMARK_FUNC(only_find_all, wal_off, disk_off);
+BENCHMARK_FUNC(only_find_eq, wal_off, disk_off);
+BENCHMARK_FUNC(only_find_gt, wal_off, disk_off);
 
 #ifdef test_with_disk
-BENCHMARK_FUNC(only_find_all, disk_on);
-BENCHMARK_FUNC(only_find_eq, disk_on);
-BENCHMARK_FUNC(only_find_gt, disk_on);
+BENCHMARK_FUNC(only_find_all, wal_on, disk_on);
+BENCHMARK_FUNC(only_find_eq, wal_on, disk_on);
+BENCHMARK_FUNC(only_find_gt, wal_on, disk_on);
 #endif
 
 int main(int argc, char** argv) {
@@ -73,9 +75,9 @@ int main(int argc, char** argv) {
     if (::benchmark::ReportUnrecognizedArguments(argc, argv)) {
         return 1;
     }
-    init_spaces<disk_off>();
+    init_spaces<wal_off, disk_off>();
 #ifdef test_with_disk
-    init_spaces<disk_on>();
+    init_spaces<wal_on, disk_on>();
 #endif
     ::benchmark::RunSpecifiedBenchmarks();
     ::benchmark::Shutdown();
