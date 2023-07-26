1e07e5063bdca4b415494e7457c336cc4d960aeb
makeyavelly <nikita.makeenkov@gmail.com>
2023-07-26 23:18:47 +0300

    add python-test SQL

--- integration/python/ottergon/__init__.py
+++ integration/python/ottergon/__init__.py
@@ -1,1 +1,1 @@
-from .ottergon import ObjectId, Client, DataBase, Collection, to_aggregate, TypeIndex, CompareIndex
+from .ottergon import ObjectId, Client, DataBase, Collection, to_aggregate, TypeIndex, CompareIndex, Result
