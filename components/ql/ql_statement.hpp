3c30e0f5a1d2d036f0c4e25fa9a2ec25035fb862
makeyavelly <nikita.makeenkov@gmail.com>
2023-08-30 22:24:20 +0300

    add ql_base_statement

--- components/ql/ql_statement.hpp
+++ components/ql/ql_statement.hpp
@@ -22,19 +22,19 @@ namespace components::ql {
         update_many,
         create_index,
         drop_index,
-        aggregate
+        aggregate,
+        join
     };
 
 
     // Base struct for every QL statement
-    struct ql_statement_t : public boost::intrusive_ref_counter<ql_statement_t> {
-        ql_statement_t(statement_type type, database_name_t database, collection_name_t collection)
-            : type_(type)
-            , database_(std::move(database))
-            , collection_(std::move(collection)) {}
+    struct ql_base_statement_t : public boost::intrusive_ref_counter<ql_base_statement_t> {
+        ql_base_statement_t(statement_type type)
+            : type_(type) {}
 
-        ql_statement_t() = default;
-        virtual ~ql_statement_t() = default;
+        ql_base_statement_t() = default;
+
+        virtual ~ql_base_statement_t() = default;
 
         statement_type type() const {
             return type_;
@@ -45,20 +45,35 @@ namespace components::ql {
         }
 
         virtual std::string to_string() const {
+            return "";
+        }
+
+        statement_type type_ {statement_type::unused};
+    };
+
+
+    struct ql_statement_t : public ql_base_statement_t {
+        ql_statement_t(statement_type type, database_name_t database, collection_name_t collection)
+            : ql_base_statement_t(type)
+            , database_(std::move(database))
+            , collection_(std::move(collection)) {}
+
+        ql_statement_t() = default;
+
+        virtual std::string to_string() const {
             std::stringstream s;
             s << "ql: " << database_ << "." << collection_;
             return s.str();
         }
 
-        statement_type type_ {statement_type::unused};
         database_name_t database_;
         collection_name_t collection_;
     };
 
 
-    struct unused_statement_t : public ql_statement_t {
+    struct unused_statement_t : public ql_base_statement_t {
         unused_statement_t()
-            : ql_statement_t(statement_type::unused, "", "") {}
+            : ql_base_statement_t(statement_type::unused) {}
     };
 
 } // namespace components::ql
