bb6812b0e818ff8537f76c5f35bf88551e65ec12
makeyavelly <nikita.makeenkov@gmail.com>
2023-08-31 22:58:14 +0300

    add nodes to create|drop operations

---
+++ components/logical_plan/node_drop_collection.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <components/ql/statements/drop_collection.hpp>
+#include "node.hpp"
+
+namespace components::logical_plan {
+
+    class node_drop_collection_t final : public node_t {
+    public:
+        explicit node_drop_collection_t(std::pmr::memory_resource* resource,
+                                        const collection_full_name_t& collection);
+
+    private:
+        hash_t hash_impl() const final;
+        std::string to_string_impl() const final;
+    };
+
+
+    node_ptr make_node_drop_collection(std::pmr::memory_resource* resource,
+                                       ql::drop_collection_t* ql);
+
+} // namespace components::logical_plan
