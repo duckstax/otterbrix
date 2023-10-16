c52e648d2c7e555499daa439aa1d92cdbe48ab2c
kotbegemot <k0tb9g9m0t@gmail.com>
2023-10-16 22:31:19 +0300

    wip

---
+++ integration/c/ottergon.h
@@ -0,0 +1,15 @@
+#ifndef OTTERGON_OTTERGON_H
+#define OTTERGON_OTTERGON_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void ottergon_init();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //OTTERGON_OTTERGON_H
