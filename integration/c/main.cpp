5361b80244d8f37ea432b58bb0887e7042e30188
kotbegemot <k0tb9g9m0t@gmail.com>
2023-10-21 11:40:01 +0300

    wip

--- integration/c/main.cpp
+++ integration/c/main.cpp
@@ -1,6 +1,4 @@
-extern "C" {
-    #include "ottergon.h"
-}
+#include "ottergon.h"
 
 #include <integration/cpp/base_spaces.hpp>
 
@@ -9,7 +7,7 @@ namespace {
         return configuration::config::default_config();
     }
 
-    struct spaces_t : public base_spaces {
+    struct spaces_t final : public duck_charmer::base_spaces {
     public:
         spaces_t(spaces_t& other) = delete;
         void operator=(const spaces_t&) = delete;
@@ -17,14 +15,14 @@ namespace {
     };
 }
 
-
 struct pod_space_t {
     state_t state;
     std::unique_ptr<spaces_t> space;
     duck_charmer::wrapper_dispatcher_t* dispatcher;
 };
 
-ottergon_ptr create(const config_t* /*cfg*/){
+ottergon_ptr create(const config_t* cfg){
+    assert(cfg != nullptr);
     auto config = create_config();
     auto pod_space = std::make_unique<pod_space_t>();
     pod_space->space = std::make_unique<spaces_t>(config);
@@ -35,17 +33,22 @@ ottergon_ptr create(const config_t* /*cfg*/){
 
 void destroy(ottergon_ptr ptr){
     assert(ptr != nullptr);
-    assert(ptr->state == state_t::created);
-    auto pod_space = reinterpret_cast<pod_space_t*>(ptr);
-    pod_space->dispatcher = nullptr;
-    pod_space->space.reset();
-    ptr->state = state_t::destroyed;
-    delete pod_space;
+    auto* spaces = reinterpret_cast<pod_space_t*>(ptr);
+    assert(spaces->state == state_t::created);
+    spaces->dispatcher = nullptr;
+    spaces->space.reset();
+    spaces->state = state_t::destroyed;
+    delete spaces;
 
 }
 
-struct result_set_t* execute_sql(ottergon_ptr ptr, string_view_t query){
-    ///auto pod_space = reinterpret_cast<pod_space_t*>(ptr);
-    ///auto res = pod_space->dispatcher->execute_sql(session, query.str());
+struct result_set_t* execute_sql(ottergon_ptr ptr, string_view_t query_raw){
+    assert(ptr != nullptr);
+    auto pod_space = reinterpret_cast<pod_space_t*>(ptr);
+    assert(pod_space->state == state_t::created);
+    assert(query_raw.data != nullptr);
+    auto session = duck_charmer::session_id_t();
+    std::string query(query_raw.data, query_raw.size);
+    auto res = pod_space->dispatcher->execute_sql(session, query);
     ///auto r = res.get<result_insert>();
 }
