2709d46b86e324f50e80130b9cbaed87b3f2d95a
Sergei <nikseas@yandex.ru>
2023-12-04 01:08:49 +0300

    review fixes

--- integration/python/wrapper_collection.cpp
+++ integration/python/wrapper_collection.cpp
@@ -65,6 +65,10 @@ namespace ottergon {
             generate_document_id_if_not_exists(doc);
             auto session_tmp = ottergon::session_id_t();
             auto result_variant = ptr_->insert_one(session_tmp, database_, name_, doc);
+            if(result_variant.is_error()){
+                debug(log_, "wrapper_collection::insert_one has result error while insert");
+                throw std::runtime_error("wrapper_collection::insert_one error_result");
+            }
             assert(result_variant.is_type<components::result::result_insert>() && "wrapper_collection::insert_one result error");
             auto& result = result_variant.get<components::result::result_insert>();
             debug(log_, "wrapper_collection::insert_one {} inserted", result.inserted_ids().empty() ? 0 : 1);
@@ -85,6 +89,10 @@ namespace ottergon {
             }
             auto session_tmp = ottergon::session_id_t();
             auto result_variant = ptr_->insert_many(session_tmp, database_, name_, docs);
+            if(result_variant.is_error()){
+                debug(log_, "wrapper_collection::insert_many has result error while insert");
+                throw std::runtime_error("wrapper_collection::insert_many error_result");
+            }
             assert(result_variant.is_type<components::result::result_insert>() && "wrapper_collection::insert_many result error");
             auto& result = result_variant.get<components::result::result_insert>();
             debug(log_, "wrapper_collection::insert_many {} inserted", result.inserted_ids().size());
--- services/dispatcher/dispatcher.cpp
+++ services/dispatcher/dispatcher.cpp
@@ -272,6 +272,7 @@ namespace services::dispatcher {
         if (!check_load_from_wal(session)) {
             actor_zeta::send(s.address(), dispatcher_t::address(), handler_id(route::execute_ql_finish), session, result);
             remove_session(session_to_address_, session);
+            result_storage_.erase(session);
         }
     }
 
