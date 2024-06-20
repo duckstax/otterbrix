1b7a9a694317f8b2fa8b7c5dc41d5a337bd312e6
OrangeSmokingJacket <whereisgeol@gmail.com>
2024-06-20 13:55:03 +0300

    msgpack support

---
+++ components/new_document/msgpack/msgpack_encoder.cpp
@@ -0,0 +1,74 @@
+#include "msgpack_encoder.hpp"
+#include <msgpack.hpp>
+
+const value_t* to_structure_(const msgpack::object& msg_object) {
+    if (msg_object.type == msgpack::type::object_type::BOOLEAN) {
+        return document::impl::new_value(msg_object.as<bool>()).detach();
+    } else if (msg_object.type == msgpack::type::object_type::POSITIVE_INTEGER) {
+        return document::impl::new_value(msg_object.as<uint64_t>()).detach();
+    } else if (msg_object.type == msgpack::type::object_type::NEGATIVE_INTEGER) {
+        return document::impl::new_value(msg_object.as<int64_t>()).detach();
+    } else if (msg_object.type == msgpack::type::object_type::FLOAT32 ||
+               msg_object.type == msgpack::type::object_type::FLOAT64 ||
+               msg_object.type == msgpack::type::object_type::FLOAT) {
+        return document::impl::new_value(msg_object.as<double>()).detach();
+    } else if (msg_object.type == msgpack::type::object_type::STR) {
+        return document::impl::new_value(msg_object.as<std::string>()).detach();
+    } else if (msg_object.type == msgpack::type::object_type::MAP) {
+        auto dict = dict_t::new_dict().detach();
+        msgpack::object_map msg_dict = msg_object.via.map;
+        for (uint32_t i = 0; i < msg_dict.size; ++i) {
+            dict->set(msg_dict.ptr[i].key.as<std::string>(), to_structure_(msg_dict.ptr[i].val));
+        }
+        return dict;
+    } else if (msg_object.type == msgpack::type::object_type::ARRAY) {
+        auto array = array_t::new_array().detach();
+        msgpack::object_array msg_array = msg_object.via.array;
+        for (uint32_t i = 0; i < msg_array.size; ++i) {
+            array->append(to_structure_(msg_array.ptr[i]));
+        }
+        return array;
+    }
+    return nullptr;
+}
+
+void to_msgpack_(const value_t* value, msgpack::object& o) {
+    if (value->type() == value_type::boolean) {
+        o.type = msgpack::type::BOOLEAN;
+        o.via.boolean = value->as_bool();
+    } else if (value->is_unsigned()) {
+        o.type = msgpack::type::POSITIVE_INTEGER;
+        o.via.u64 = value->as_unsigned();
+    } else if (value->is_int()) {
+        o.type = msgpack::type::NEGATIVE_INTEGER;
+        o.via.i64 = value->as_int();
+    } else if (value->is_double()) {
+        o.type = msgpack::type::FLOAT64;
+        o.via.f64 = value->as_double();
+    } else if (value->type() == value_type::string) {
+        //todo kick memory leak
+        auto* s = new std::string(to_string(value));
+        o.type = msgpack::type::object_type::STR;
+        o.via.str.size = uint32_t(s->size());
+        o.via.str.ptr = s->c_str();
+    } else if (value->type() == value_type::dict) {
+        auto* dict = value->as_dict();
+        o.type = msgpack::type::MAP;
+        o.via.map = msgpack::object_map{dict->count(), new msgpack::object_kv[dict->count()]};
+        int i = 0;
+        for (auto it = dict->begin(); it; ++it) {
+            to_msgpack_(it.key(), o.via.map.ptr[i].key);
+            to_msgpack_(it.value(), o.via.map.ptr[i].val);
+            ++i;
+        }
+    } else if (value->type() == value_type::array) {
+        auto* array = value->as_array();
+        o.type = msgpack::type::ARRAY;
+        o.via.array = msgpack::object_array{array->count(), new msgpack::object[array->count()]};
+        int i = 0;
+        for (auto it = array->begin(); it; ++it) {
+            to_msgpack_(it.value(), o.via.map.ptr[i].val);
+            ++i;
+        }
+    }
+}
---
+++ components/new_document/msgpack/msgpack_encoder.hpp
@@ -0,0 +1,81 @@
+#pragma once
+
+#include <components/document/core/array.hpp>
+#include <components/document/core/dict.hpp>
+#include <components/document/document.hpp>
+#include <components/document/document_view.hpp>
+#include <msgpack.hpp>
+
+using ::document::impl::array_t;
+using ::document::impl::dict_t;
+using ::document::impl::value_t;
+using ::document::impl::value_type;
+
+template<typename Stream>
+void to_msgpack_(msgpack::packer<Stream>& o, const value_t* value) {
+    if (value->type() == value_type::dict) {
+        auto* dict = value->as_dict();
+        o.pack_map(dict->count());
+        for (auto it = dict->begin(); it; ++it) {
+            o.pack(to_string(it.key()));
+            to_msgpack_(o, it.value());
+        }
+    } else if (value->type() == value_type::array) {
+        auto* array = value->as_array();
+        o.pack_array(array->count());
+        for (auto it = array->begin(); it; ++it) {
+            to_msgpack_(o, it.value());
+        }
+    } else if (value->type() == value_type::boolean) {
+        o.pack(value->as_bool());
+    } else if (value->is_unsigned()) {
+        o.pack(value->as_unsigned());
+    } else if (value->is_int()) {
+        o.pack(value->as_int());
+    } else if (value->is_double()) {
+        o.pack(value->as_double());
+    } else if (value->type() == value_type::string) {
+        o.pack(to_string(value));
+    }
+}
+
+const value_t* to_structure_(const msgpack::object& msg_object);
+void to_msgpack_(const value_t* value, msgpack::object& o);
+
+// User defined class template specialization
+namespace msgpack {
+    MSGPACK_API_VERSION_NAMESPACE(MSGPACK_DEFAULT_API_NS) {
+        namespace adaptor {
+
+            template<>
+            struct convert<components::document::document_ptr> final {
+                msgpack::object const& operator()(msgpack::object const& o,
+                                                  components::document::document_ptr& v) const {
+                    if (o.type != msgpack::type::MAP) {
+                        throw msgpack::type_error();
+                    }
+                    v = components::document::make_document(to_structure_(o)->as_dict());
+                    return o;
+                }
+            };
+
+            template<>
+            struct pack<components::document::document_ptr> final {
+                template<typename Stream>
+                packer<Stream>& operator()(msgpack::packer<Stream>& o,
+                                           components::document::document_ptr const& v) const {
+                    to_msgpack_(o, components::document::document_view_t(v).get_value());
+                    return o;
+                }
+            };
+
+            template<>
+            struct object_with_zone<components::document::document_ptr> final {
+                void operator()(msgpack::object::with_zone& o, components::document::document_ptr const& v) const {
+                    to_msgpack_(components::document::document_view_t(v).get_value(), o);
+                }
+            };
+
+        } // namespace adaptor
+    }     // MSGPACK_API_VERSION_NAMESPACE(MSGPACK_DEFAULT_API_NS)
+} // namespace msgpack
