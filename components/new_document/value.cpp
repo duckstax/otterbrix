6de0af841a4b4b4e87426103fcd8fc764c14b341
OrangeSmokingJacket <whereisgeol@gmail.com>
2024-06-28 18:23:52 +0200

    value_t prototype

---
+++ components/new_document/value.cpp
@@ -0,0 +1,114 @@
+#include "value.hpp"
+
+namespace components::new_document {
+
+    value_t::value_t(std::pmr::memory_resource* allocator)
+        : mut_src_(new (allocator->allocate(sizeof(impl::mutable_document))) impl::mutable_document(allocator)) {
+        builder_ = components::new_document::tape_builder<impl::tape_writer_to_mutable>(allocator, *mut_src_);
+    }
+
+    types::physical_type value_t::physical_type() const noexcept { return element_.physical_type(); }
+
+    types::logical_type value_t::logical_type() const noexcept { return element_.logical_type(); }
+
+    static inline bool is_equals(double x, double y) {
+        return std::fabs(x - y) < std::numeric_limits<double>::epsilon();
+    }
+
+    bool value_t::operator==(const value_t& rhs) const {
+        switch (physical_type()) {
+            case types::physical_type::UINT8:
+            case types::physical_type::UINT16:
+            case types::physical_type::UINT32:
+            case types::physical_type::UINT64:
+                return element_.get_uint64().value() == rhs.element_.get_uint64().value();
+            case types::physical_type::INT8:
+            case types::physical_type::INT16:
+            case types::physical_type::INT32:
+            case types::physical_type::INT64:
+                return element_.get_int64().value() == rhs.element_.get_int64().value();
+            case types::physical_type::UINT128:
+            case types::physical_type::INT128:
+                return element_.get_int128().value() == rhs.element_.get_int128().value();
+            case types::physical_type::FLOAT:
+            case types::physical_type::DOUBLE:
+                return is_equals(element_.get_double().value(), rhs.element_.get_double().value());
+            case types::physical_type::STRING:
+                return element_.get_string().value() == rhs.element_.get_string().value();
+            default: // special values are always equal
+                return physical_type() == rhs.physical_type();
+        }
+    }
+
+    bool value_t::operator<(const value_t& rhs) const {
+        switch (physical_type()) {
+            case types::physical_type::BOOL_FALSE:
+            case types::physical_type::BOOL_TRUE:
+                return element_.get_bool().value() < rhs.element_.get_bool().value();
+            case types::physical_type::UINT8:
+            case types::physical_type::UINT16:
+            case types::physical_type::UINT32:
+            case types::physical_type::UINT64:
+                return element_.get_uint64().value() < rhs.element_.get_uint64().value();
+            case types::physical_type::INT8:
+            case types::physical_type::INT16:
+            case types::physical_type::INT32:
+            case types::physical_type::INT64:
+                return element_.get_int64().value() < rhs.element_.get_int64().value();
+            case types::physical_type::UINT128:
+            case types::physical_type::INT128:
+                return element_.get_int128().value() < rhs.element_.get_int128().value();
+            case types::physical_type::FLOAT:
+            case types::physical_type::DOUBLE:
+                return element_.get_double().value() < rhs.element_.get_double().value();
+            case types::physical_type::STRING:
+                return element_.get_string().value() < rhs.element_.get_string().value();
+            default: // special values are always equal
+                return false;
+        }
+    }
+
+    bool value_t::as_bool() const noexcept { return element_.get_bool().value(); }
+
+    int64_t value_t::as_int() const noexcept { return element_.get_int64().value(); }
+
+    uint64_t value_t::as_unsigned() const noexcept { return element_.get_uint64().value(); }
+
+    float value_t::as_float() const noexcept { return element_.get_float().value(); }
+
+    double value_t::as_double() const noexcept { return element_.get_double().value(); }
+
+    std::string_view value_t::as_string() const noexcept { return element_.get_string().value(); }
+
+    bool value_t::is_int() const noexcept {
+        switch (physical_type()) {
+            case types::physical_type::INT8:
+            case types::physical_type::INT16:
+            case types::physical_type::INT32:
+            case types::physical_type::INT64:
+            case types::physical_type::INT128:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    bool value_t::is_unsigned() const noexcept {
+        switch (physical_type()) {
+            case types::physical_type::UINT8:
+            case types::physical_type::UINT16:
+            case types::physical_type::UINT32:
+            case types::physical_type::UINT64:
+            case types::physical_type::UINT128:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    bool value_t::is_double() const noexcept {
+        return physical_type() == types::physical_type::FLOAT || physical_type() == types::physical_type::DOUBLE;
+    }
+
+    bool value_t::is_string() const noexcept { return physical_type() == types::physical_type::STRING; }
+} // namespace components::new_document
