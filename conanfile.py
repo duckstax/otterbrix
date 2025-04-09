d0fa2e781f3b12926124a35aed7dea38ae211a53
OrangeSmokingJacket <whereisgeol@gmail.com>
2025-04-09 17:22:26 +0200

    update

--- conanfile.py
+++ conanfile.py
@@ -20,7 +20,7 @@ class MyProjectConan(ConanFile):
         self.requires("zlib/1.2.12@")
         self.requires("bzip2/1.0.8@")
         self.requires("magic_enum/0.8.1@")
-        self.requires("actor-zeta/1.0.0a11@duckstax/stable")
+        self.requires("actor-zeta/1.0.0a12@otterbrix/stable")
 
     options = {
         "actor-zeta/*:cxx_standard": [17],
